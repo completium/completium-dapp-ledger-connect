3ec18392d18f7bd818588e3ceb27a482a2ba01ff
Guillaume Duhamel <guillaume.duhamel@edukera.com>
2021-01-12 19:29:12 +0100

    up

--- src/components/LedgerConnect.js
+++ src/components/LedgerConnect.js
@@ -2,40 +2,58 @@ import React from 'react';
 import Button from '@material-ui/core/Button';
 import { LedgerSigner, DerivationType } from '@taquito/ledger-signer';
 import TransportU2F from "@ledgerhq/hw-transport-u2f";
-import { TezosToolkit } from "@taquito/taquito";
+import TextField from '@material-ui/core/TextField';
 
 export default function LedgerConnect(props) {
-  const [ready, setReady] = React.useState(false);
-
   const { tezos, handleSelected } = props;
-  // const createTransport = () => {
-  //   try {
-  //     const transport = await TransportU2F.create();
-  //     setReady(true);
-  //     return transport;
-  //   } catch (e) {
-  //     console.log(e);
-  //   }
-  // }
 
-  const updateTezos = (accountId = 0, changeId = 0, dt = DerivationType.ED25519) => {
-    // const transport = await TransportU2F.create();
-    // const ledgerSigner = new LedgerSigner(transport, `44'/1729'/${accountId}'/${changeId}'`, true, dt);
-    // tezos.setProvider({ signer: ledgerSigner });
-    // const publicKeyHash = await tezos.signer.publicKeyHash();
-    const publicKeyHash = 'tz1bivjTpG3xTvcJTKnu1kVK1Fmp8gA9mV8w';
+  const [connected, setConnected] = React.useState(false);
+  const [transport, setTransport] = React.useState(false);
+  const [error, setError] = React.useState(null);
+  const [accountId, setAccountId] = React.useState("0");
+  const [changeId, setChangeId] = React.useState("0");
+
+
+  const initTransport = async () => {
+    try {
+      const tr = await TransportU2F.create();
+      setTransport(tr);
+      setConnected(true);
+    } catch (e) {
+      console.log(e);
+      setError(e);
+    }
+  }
+
+  const updateTezos = async (accountId = 0, changeId = 0, dt = DerivationType.ED25519) => {
+    const ledgerSigner = new LedgerSigner(transport, `44'/1729'/${accountId}'/${changeId}'`, true, dt);
+    tezos.setProvider({ signer: ledgerSigner });
+    const publicKeyHash = await tezos.signer.publicKeyHash();
 
     handleSelected(publicKeyHash);
   }
 
+  const handletextfield = (event) => {
+    var value = event.target.value;
+    switch (event.target.id) {
+      case 'accountId'         : setAccountId(value); break;
+      case 'changeId'          : setChangeId(value); break;
+      default : {}
+    }
+  };
+
   const handleClickOpen = () => {
-    updateTezos();
+    updateTezos(accountId, changeId);
   }
 
   return (
-    <div>
-      <Button onClick={handleClickOpen}> Select </Button>
-    </div>
-
+    (connected ? (<div>
+      <form noValidate autoComplete="off">
+        <span>44'/1729'/</span>
+        <TextField id="accountId" label="accountId" defaultValue="0"  onChange={handletextfield} />
+        <span>/</span>
+        <TextField id="changeId" label="changeId" defaultValue="0"  onChange={handletextfield} />
+      </form>
+      <Button onClick={handleClickOpen}> Select address </Button></div>) : (<Button onClick={initTransport}> Connect ledger </Button>))
   );
 }
